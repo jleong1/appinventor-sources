acbcfb16caca9b73bc378f11b22443641864de1d
Julie Leong <jleong1@luc.edu>
2015-11-18 22:00:55 -0600

    No new changes, but computer wanted to commit

--- appinventor/lib/blockly/src/core/blockly.js
+++ appinventor/lib/blockly/src/core/blockly.js
@@ -358,20 +358,15 @@ Blockly.onMouseDown_ = function(e) {
 /*
     var canvas = Blockly.mainWorkspace.getCanvas;
     var ctx;
-
     var canvasOffset;
     var offsetX;
     var offsetY;
-
     var isDrawing = false;
-
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-
     canvasOffset = $("#canvas").offset();
     offsetX = canvasOffset.left;
     offsetY = canvasOffset.top;
-
     $("#canvas").on('mousedown', function (e) {
         handleMouseDown(e);
     }).on('mouseup', function(e) {
@@ -379,29 +374,22 @@ Blockly.onMouseDown_ = function(e) {
     }).on('mousemove', function(e) {
         handleMouseMove(e);
     });
-
-
     var startX;
     var startY;
-
     function handleMouseUp() {
     	isDrawing = false;
     	canvas.style.cursor = "default";
     }
-
     function handleMouseMove(e) {
     	if (isDrawing) {
     		var mouseX = parseInt(e.clientX - offsetX);
     		var mouseY = parseInt(e.clientY - offsetY);
-
     		ctx.clearRect(0, 0, canvas.width, canvas.height);
     		ctx.beginPath();
     		ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
     		ctx.stroke();
-
     	}
     }
-
     function handleMouseDown(e) {
     	canvas.style.cursor = "crosshair";
     	isDrawing = true
